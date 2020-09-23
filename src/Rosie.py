5940b379ef94f2a9a34f0108476bbad7ab32395a
Tejas-Nanaware <nanawaret@hotmail.com>
2020-09-23 14:33:52 -0500

    Using all AIML Rosie files

--- src/Rosie.py
+++ src/Rosie.py
@@ -3,7 +3,7 @@ import aiml
 
 # Create the kernel and learn AIML files
 kernel = aiml.Kernel()
-kernel.learn("./rosie/lib/rosie_startup.xml")
+kernel.learn("./rosie_startup.xml")
 kernel.respond("load aiml b")
 
 # Press CTRL-C to break this loop
