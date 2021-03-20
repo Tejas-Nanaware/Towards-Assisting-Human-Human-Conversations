cdf5812dadc61e4df0296ec0777708aa8ce1e96b
Tejas-Nanaware <nanawaret@hotmail.com>
2021-03-19 20:47:10 -0500

    Deleted unnecessary files about old version of chat framework

--- src/OldChatFramework/controllers/index.js
+++
@@ -1,8 +0,0 @@
-const index = (req, res, next) => {
-    console.log("index is called");
-    res.sendFile(rootDir + '/public/index.html');
-}
-
-module.exports = {
-    index: index
-}
--- src/OldChatFramework/sequelize/extra-setup.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require(".");
-
-const applyExtraSetup = (sequelize) => {
-    // const { login, message } = sequelize.models;
-    // message.hasMany(login);
-}
-
-module.exports = { applyExtraSetup };
