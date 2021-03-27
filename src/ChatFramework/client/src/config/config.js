2690e9a06f5babd42fbd0160c1fb2124e6f5b1d8
Tejas-Nanaware <nanawaret@hotmail.com>
2021-03-26 21:45:07 -0500

    Fixed socket path for aws deployment

--- src/ChatFramework/client/src/config/config.js
+++ src/ChatFramework/client/src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   node_server: 'http://3.134.130.112/chatapi/',
-  socket_server: 'http://3.134.130.112/chatapi/',
+  socket_server: 'http://3.134.130.112',
   socket_path: '/chatapi/socket.io',
   blenderBotURL: 'http://3.134.130.112/flaskapi/api/v1/Blenderbot?text=',
   dialoGPTURL: 'http://3.134.130.112/flaskapi/api/v1/DialoGPT?text='
